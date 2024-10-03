edd44804a12ae11da01e39a0e0eb1c972326860e
Gabriel Madalena dos Santos <110430707+Tolksie@users.noreply.github.com>
2024-10-03 00:15:56 +0000

    first

---
+++ microservfinanceiro/src/main/java/br/univille/microservfinanceiro/MicroservfinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservfinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservfinanceiroApplication.class, args);
+	}
+
+}
---
+++ microservfinanceiro/src/test/java/br/univille/microservfinanceiro/MicroservfinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservfinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
